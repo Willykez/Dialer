26d8a992f9ee3b9a0f6bed36625a44870f14072b
Repo Master User <repomaster@users.noreply.github.com>
2026-07-23 06:45:01 +0300

    Update DialpadScreen.kt

--- app/src/main/java/com/willykez/dialer/ui/dialpad/DialpadScreen.kt
+++ app/src/main/java/com/willykez/dialer/ui/dialpad/DialpadScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -233,7 +232,7 @@ fun DialpadScreen(
                     tint = Color.White
                 )
             }
-
+    
             Box(
                 modifier = Modifier.size(76.dp),
                 contentAlignment = Alignment.Center
